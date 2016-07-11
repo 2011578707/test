7468dd4aba2ce624c5d06034094edc3bd446598f
942391815@qq.com <942391815@qq.com>
2016-07-11 22:24:30 +0800

    test

---
+++ test/src/main/java/com/test/java/Test2.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+import java.util.Date;
+
+/**
+ * Created by Micheal on 2016/7/11.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
