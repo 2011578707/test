feef147853cd1d5e4266519bc983b62744c1a2cf
942391815@qq.com <942391815@qq.com>
2016-07-11 22:23:11 +0800

    Initial commit

---
+++ test/src/main/java/com/test/java/Test.java
@@ -0,0 +1,10 @@
+package com.test.java;
+
+/**
+ * Created by Micheal on 2016/7/11.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
